46ad225c6b4a28aed2bba29707834d3c3589c19e
James Bibby <bibby@udacity.com>
2017-08-04 12:41:06 -0400

    adding forms and server functions

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import escapeRegExp from "escape-string-regexp";
 import sortBy from "sort-by";
+import { Link } from "react-router-dom";
 
 class ListContacts extends Component {
   static propTypes = {
@@ -45,6 +46,9 @@ class ListContacts extends Component {
             value={query}
             onChange={event => this.updateQuery(event.target.value)}
           />
+          <Link to="/create" className="add-contact">
+            Add Contact
+          </Link>
         </div>
         {showingContacts.length !== contacts.length &&
           <div className="showing-contacts">
