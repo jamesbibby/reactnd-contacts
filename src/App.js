d86c5275c8f42845dc4d13dcf6000fe2d2570b50
James Bibby <bibby@udacity.com>
2017-08-04 13:01:08 -0400

    fixed my linting rules

--- src/App.js
+++ src/App.js
@@ -1,58 +1,58 @@
-import React, { Component } from "react";
-import { Route } from "react-router-dom";
-import ListContacts from "./ListContacts";
-import * as ContactsAPI from "./utils/ContactsAPI";
-import CreateContact from "./CreateContact";
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import ListContacts from './ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI';
+import CreateContact from './CreateContact';
 
 class App extends Component {
-  state = {
-    contacts: []
-  };
+	state = {
+		contacts: [],
+	};
 
-  componentDidMount() {
-    ContactsAPI.getAll().then(contacts => this.setState({ contacts }));
-  }
+	componentDidMount() {
+		ContactsAPI.getAll().then(contacts => this.setState({ contacts }));
+	}
 
-  removeContact = contact => {
-    this.setState(state => ({
-      contacts: state.contacts.filter(current => contact.id !== current.id)
-    }));
-    ContactsAPI.remove(contact);
-  };
+	removeContact = contact => {
+		this.setState(state => ({
+			contacts: state.contacts.filter(current => contact.id !== current.id),
+		}));
+		ContactsAPI.remove(contact);
+	};
 
-  createContact = contact => {
-    ContactsAPI.create(contact).then(contact => {
-      this.setState(state => ({
-        contacts: state.contacts.concat([contact])
-      }));
-    });
-  };
+	createContact = contact => {
+		ContactsAPI.create(contact).then(contact => {
+			this.setState(state => ({
+				contacts: state.contacts.concat([contact]),
+			}));
+		});
+	};
 
-  render() {
-    return (
-      <div>
-        <Route
-          exact
-          path="/"
-          render={() =>
-            <ListContacts
-              contacts={this.state.contacts}
-              onDeleteContact={this.removeContact}
-            />}
-        />
-        <Route
-          path="/create"
-          render={({ history }) =>
-            <CreateContact
-              onCreateContact={contact => {
-                this.createContact(contact);
-                history.push("/");
-              }}
-            />}
-        />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Route
+					exact
+					path="/"
+					render={() =>
+						<ListContacts
+							contacts={this.state.contacts}
+							onDeleteContact={this.removeContact}
+						/>}
+				/>
+				<Route
+					path="/create"
+					render={({ history }) =>
+						<CreateContact
+							onCreateContact={contact => {
+								this.createContact(contact);
+								history.push('/');
+							}}
+						/>}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,89 +1,89 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import escapeRegExp from "escape-string-regexp";
-import sortBy from "sort-by";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by';
+import { Link } from 'react-router-dom';
 
 class ListContacts extends Component {
-  static propTypes = {
-    contacts: PropTypes.array.isRequired,
-    onDeleteContact: PropTypes.func.isRequired
-  };
+	static propTypes = {
+		contacts: PropTypes.array.isRequired,
+		onDeleteContact: PropTypes.func.isRequired,
+	};
 
-  state = {
-    query: ""
-  };
+	state = {
+		query: '',
+	};
 
-  updateQuery(query) {
-    this.setState({ query: query.trim() });
-  }
+	updateQuery(query) {
+		this.setState({ query: query.trim() });
+	}
 
-  clearQuery() {
-    this.setState({ query: "" });
-  }
+	clearQuery() {
+		this.setState({ query: '' });
+	}
 
-  render() {
-    let showingContacts;
-    const { query } = this.state;
-    const { contacts, onDeleteContact } = this.props;
+	render() {
+		let showingContacts;
+		const { query } = this.state;
+		const { contacts, onDeleteContact } = this.props;
 
-    if (query) {
-      const match = new RegExp(escapeRegExp(query), "i");
-      showingContacts = contacts.filter(contact => match.test(contact.name));
-    } else {
-      showingContacts = contacts;
-    }
+		if (query) {
+			const match = new RegExp(escapeRegExp(query), 'i');
+			showingContacts = contacts.filter(contact => match.test(contact.name));
+		} else {
+			showingContacts = contacts;
+		}
 
-    showingContacts.sort(sortBy("name"));
+		showingContacts.sort(sortBy('name'));
 
-    return (
-      <div className="list-contacts">
-        <div className="list-contacts-top">
-          <input
-            className="search-contacts"
-            type="text"
-            placeholder="Search contacts"
-            value={query}
-            onChange={event => this.updateQuery(event.target.value)}
-          />
-          <Link to="/create" className="add-contact">
-            Add Contact
-          </Link>
-        </div>
-        {showingContacts.length !== contacts.length &&
-          <div className="showing-contacts">
-            Now showing {showingContacts.length} of {contacts.length}.
-            <button onClick={event => this.clearQuery()}>Show All</button>
-          </div>}
-        <ol className="contact-list">
-          {showingContacts.map(contact =>
-            <li key={contact.id} className="contact-list-item">
-              <div
-                className="contact-avatar"
-                style={{
-                  backgroundImage: `url(${contact.avatarURL})`
-                }}
-              />
-              <div className="contact-details">
-                <p>
-                  {contact.name}
-                </p>
-                <p>
-                  {contact.email}
-                </p>
-              </div>
-              <button
-                className="contact-remove"
-                onClick={() => onDeleteContact(contact)}
-              >
-                Remove
-              </button>
-            </li>
-          )}
-        </ol>
-      </div>
-    );
-  }
+		return (
+			<div className="list-contacts">
+				<div className="list-contacts-top">
+					<input
+						className="search-contacts"
+						type="text"
+						placeholder="Search contacts"
+						value={query}
+						onChange={event => this.updateQuery(event.target.value)}
+					/>
+					<Link to="/create" className="add-contact">
+						Add Contact
+					</Link>
+				</div>
+				{showingContacts.length !== contacts.length &&
+					<div className="showing-contacts">
+						Now showing {showingContacts.length} of {contacts.length}.
+						<button onClick={event => this.clearQuery()}>Show All</button>
+					</div>}
+				<ol className="contact-list">
+					{showingContacts.map(contact =>
+						<li key={contact.id} className="contact-list-item">
+							<div
+								className="contact-avatar"
+								style={{
+									backgroundImage: `url(${contact.avatarURL})`,
+								}}
+							/>
+							<div className="contact-details">
+								<p>
+									{contact.name}
+								</p>
+								<p>
+									{contact.email}
+								</p>
+							</div>
+							<button
+								className="contact-remove"
+								onClick={() => onDeleteContact(contact)}
+							>
+								Remove
+							</button>
+						</li>
+					)}
+				</ol>
+			</div>
+		);
+	}
 }
 
 export default ListContacts;
