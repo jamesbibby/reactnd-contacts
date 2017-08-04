5cd8a7d0e3fdac627a0d9e329b72024b0324f501
James Bibby <bibby@udacity.com>
2017-08-04 10:29:19 -0400

    End of State Management

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from "react";
-
-class ContactList extends Component {
-  render() {
-    const people = [{ name: "Daenerys" }, { name: "Jon" }, { name: "Arya" }];
-    return (
-      <ol>
-        {people.map(person =>
-          <li>
-            {person.name}
-          </li>
-        )}
-      </ol>
-    );
-  }
-}
+import ListContacts from "./ListContacts";
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: "ryan",
+        name: "Ryan Florence",
+        email: "ryan@reacttraining.com",
+        avatarURL: "http://localhost:5001/ryan.jpg"
+      },
+      {
+        id: "michael",
+        name: "Michael Jackson",
+        email: "michael@reacttraining.com",
+        avatarURL: "http://localhost:5001/michael.jpg"
+      },
+      {
+        id: "tyler",
+        name: "Tyler McGinnis",
+        email: "tyler@reacttraining.com",
+        avatarURL: "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  };
+
+  removeContact = contact => {
+    this.setState(state => ({
+      contacts: state.contacts.filter(current => contact.id !== current.id)
+    }));
+  };
+
   render() {
     return (
-      <div className="App">
-        <ContactList />
+      <div>
+        <ListContacts
+          contacts={this.state.contacts}
+          onDeleteContact={this.removeContact}
+        />
       </div>
     );
   }
